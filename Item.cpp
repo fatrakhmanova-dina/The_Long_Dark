c5234d69c23880b74eb89dfd1197f55ae68679f1
Fatrakhmanova Dina <fatrakhmanova.dk@phystech.edu>
2025-02-11 20:09:44 +0300

    Add files via upload

--- Item.cpp
+++ Item.cpp
@@ -4,32 +4,11 @@ using namespace std;
 
 class Item
 {
-public:
-    virtual getWeight() = 0;
-    virtual getType() = 0; // предлагаю то разделение которое мы придумали ввести как характеристику, как будто бы это может пригодиться потом
-    virtual getQuality() = 0;
-
-};
-
-
-class Food: public Item
-{
 protected:
     int weight; //Или флоат сделать? Просто хз не возникнет ли проблем со сравнениием, а в интах все держать явно проще
     int quality;
     int type;//Я хотела сделать стринг, но получается хуйня, так что надо будет сделать таблицу с номерами и тем, какой тип им соотвествует
-    int food_value;
-    int water_value;
 public:
-    Food(int w, int q, int t, int fv, int wv)
-    {
-        this->weight = w;
-        this->quality = q;
-        this->type = t;
-        this->food_value = fv;
-        this->water_value = wv;
-    }
-    ~Food(){}
     int getWeight()
     {
         return this->weight;
@@ -42,6 +21,26 @@ public:
     {
         return this->quality;
     }
+
+};
+
+
+class Food: public Item
+{
+protected:
+    int food_value;
+    int water_value;
+public:
+    Food(int w, int q, int t, int fv, int wv)
+    {
+        this->weight = w;
+        this->quality = q;
+        this->type = t;
+        this->food_value = fv;
+        this->water_value = wv;
+    }
+    ~Food(){}
+
     int getFood_val()
     {
         return this->food_value;
@@ -56,11 +55,22 @@ class Canned_foods: protected Food
 {
     bool canOpen(); //Функция, проверяющая игрока на наличие нужного инструмента чтобы открыть. Сейчас не могу написать, т к нет класса игрока
 };
+/*
+class Carcass: protected Item
+{
+protected:
+    int freeze; // величина от 0 до 1 - процент заморозки
+    int meat;
+    int gut;
+    int skin;
+public:
 
+
+};*/
 
 int main()
 {
-    Food f(10, 1, 11, 30, 15);
+    Canned_foods f(10, 1, 11, 30, 15);
     cout << f.getType() << endl;
     cout << f.getWeight() << endl;
     return 0;
