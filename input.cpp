98f9c361f5aae376f92597314d7761daa9685fe7
Fatrakhmanova Dina <fatrakhmanova.dk@phystech.edu>
2025-05-16 23:56:24 +0300

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,47 @@
+#include "Engine.h"
+
+void Engine::input()
+{
+	//Event variable
+	Event event;
+
+	while (m_Window.pollEvent(event))
+	{
+        //Allow player to move using WASD
+        if (Keyboard::isKeyPressed(Keyboard::W))
+        {
+            pPlayer->moveUp();
+        }
+        else
+        {
+            pPlayer->stopUp();
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::S))
+        {
+            pPlayer->moveDown();
+        }
+        else
+        {
+            pPlayer->stopDown();
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::A))
+        {
+            pPlayer->moveLeft();
+        }
+        else
+        {
+            pPlayer->stopLeft();
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::D))
+        {
+            pPlayer->moveRight();
+        }
+        else
+        {
+            pPlayer->stopRight();
+        }
+	}
+}
